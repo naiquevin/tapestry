b757f4e3c1ebc5be277258c473523eec5c78a9cc
Vineet Naik <naikvin@gmail.com>
2024-03-27 15:20:28 +0530

    Get basic template rendering working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use minijinja::context;
+
 fn main() {
-    println!("Hello, world!");
+    let mut tmpl_env = minijinja::Environment::new();
+    tmpl_env.set_loader(minijinja::path_loader("/Users/vineet/code/dastanu/db/sql_templates"));
+    let tmpl = tmpl_env.get_template("q_shoplist.sql.j2").unwrap();
+    let ctx = context! { cond__catid => true };
+    let output = tmpl.render(ctx).unwrap();
+    println!("{output:}");
 }
