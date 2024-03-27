28bb1f400d258da593ab8ea247de8f754024b8d2
Vineet Naik <naikvin@gmail.com>
2024-03-27 18:26:21 +0530

    Add POC for double template evaluation
    In the original template, the placeholders for positional arguments or
    named variables (as per user's choice) are to be specified using the
    'placeholder' function added to the template environment.
    
    Double template evaluation means the input template will be evaluated
    twice subsequently.
    
    In the first step, all variables and function calls will be
    evaluated. Variables are expected to be of type boolean with var names
    prefixed with 'cond__' as a convention. The placeholder function calls
    will get evaluated to jinja expressions. Example:
    
    {{ placeholder('myvar') }} will get evaluated to '{{ myvar }}'
    
    In other words, the output of first rendering is also a jinja
    template.
    
    This intermediate representation can now be rendered into a query that
    can either be,
    
    1. used in production code (with positional arguments or
       named variables) or,
    
    2. used to generate pgTap tests (POC pending)
    
    An example template is also added. The query is based on the Chinook
    database[1].
    
    [1]: https://github.com/lerocha/chinook-database

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,47 @@
-use minijinja::context;
+use minijinja::{context, Error};
+
+fn placeholder(name: String) -> Result<String, Error> {
+    Ok(format!("{{{{ {name} }}}}"))
+}
 
 fn main() {
     let mut tmpl_env = minijinja::Environment::new();
-    tmpl_env.set_loader(minijinja::path_loader("/Users/vineet/code/dastanu/db/sql_templates"));
-    let tmpl = tmpl_env.get_template("q_shoplist.sql.j2").unwrap();
-    let ctx = context! { cond__catid => true };
+    tmpl_env.set_loader(minijinja::path_loader("examples"));
+    tmpl_env.add_function("placeholder", placeholder);
+
+    let tmpl = tmpl_env.get_template("songs.sql.j2").unwrap();
+    let ctx = context! { cond__artist => true, cond__file_format => true };
     let output = tmpl.render(ctx).unwrap();
-    println!("{output:}");
+    println!("-- Intermediate query");
+    println!("{output}");
+    println!("");
+
+    // The generated output is again a jinja2 template. We add it to
+    // the env and find the undeclared_variables
+    tmpl_env.add_template("_gen1", &output).unwrap();
+    let gen_tmpl = tmpl_env.get_template("_gen1").unwrap();
+    let udvars = gen_tmpl.undeclared_variables(false);
+    println!("-- {udvars:?}");
+    println!("");
+
+    // @NOTE that the query for production will *NOT* be constructed
+    // as follows. Instead the intermediate query will be parsed and
+    // the using with the `undeclared_variables`, the placeholders in
+    // the intermediate query will be mapped to the positional
+    // arguments automatically.
+    let query = gen_tmpl.render(context! {
+        artist => "$1",
+        file_format => "$2"
+    }).unwrap();
+    println!("-- Query for production code");
+    println!("{query}");
+    println!("");
+
+    let test_query = gen_tmpl.render(context! {
+        artist => "'Iron Maiden'",
+        file_format => "'Protected AAC audio file'"
+    }).unwrap();
+    println!("-- Query for test code");
+    println!("{test_query}");
+    println!("");
 }
