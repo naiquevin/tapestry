f9965c928c26e787610848265ddcfb7a1e9576f4
Vineet Naik <naikvin@gmail.com>
2024-05-07 12:19:39 +0530

    Refactor to compute pg_format args only once

--- src/sql_format.rs
+++ src/sql_format.rs
@@ -44,7 +44,16 @@ impl<'a> Cmd<'a> {
 #[derive(Debug)]
 pub struct PgFormatter {
     exec_path: PathBuf,
-    conf_path: Option<PathBuf>,
+    _conf_path: Option<PathBuf>,
+    args: Vec<String>,
+}
+
+fn pg_format_args(conf_path: Option<&Path>) -> Vec<String> {
+    let args = match conf_path {
+        Some(conf) => vec!["-c", conf.to_str().unwrap(), "-"],
+        None => vec!["-M", "-p", "start\\(noformat\\).+end\\(noformat\\)", "-"],
+    };
+    args.into_iter().map(String::from).collect()
 }
 
 impl TryFrom<&Value> for PgFormatter {
@@ -59,13 +68,15 @@ impl TryFrom<&Value> for PgFormatter {
                         "Missing 'exec_path' in 'formatter.pgFormatter"
                     ))
                     .map(|v| decode_pathbuf(v, None, "formatter.pgFormatter.exec_path"))??;
-                let conf_path = match t.get("conf_path") {
+                let _conf_path = match t.get("conf_path") {
                     Some(cp) => Some(decode_pathbuf(cp, None, "formatter.pgFormatter.conf_path")?),
                     None => None,
                 };
+                let args = pg_format_args(_conf_path.as_ref().map(|p| p.as_path()));
                 Ok(Self {
                     exec_path,
-                    conf_path,
+                    _conf_path,
+                    args,
                 })
             }
             None => Err(parse_error!(
@@ -76,19 +87,9 @@ impl TryFrom<&Value> for PgFormatter {
 }
 
 impl PgFormatter {
+
     pub fn format(&self, sql: &str) -> Vec<u8> {
-        let mut default_args = vec!["-M", "-p", "start\\(noformat\\).+end\\(noformat\\)", "-"];
-        let mut args = Vec::new();
-        match &self.conf_path {
-            Some(conf) => {
-                args.push("-c");
-                args.push(conf.to_str().unwrap());
-                args.push("-");
-            }
-            None => {
-                args.append(&mut default_args);
-            }
-        }
+        let args = self.args.iter().map(|a| a.as_str()).collect();
         let cmd = Cmd {
             exec: &self.exec_path,
             args,
@@ -125,5 +126,24 @@ impl Formatter {
         match self {
             Self::PgFormatter(p) => p.format(sql),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::pg_format_args;
+    use std::path::Path;
+
+    #[test]
+    fn test_pg_format_args() {
+        let args = pg_format_args(None);
+        let expected = vec!["-M", "-p", "start\\(noformat\\).+end\\(noformat\\)", "-"];
+        assert_eq!(expected.into_iter().map(String::from).collect::<Vec<String>>(), args);
+
+        let conf_path = Path::new("./.pg_format/config");
+        let args = pg_format_args(Some(&conf_path));
+        let expected = vec!["-c", "./.pg_format/config", "-"];
+        assert_eq!(expected.into_iter().map(String::from).collect::<Vec<String>>(), args);
     }
 }
