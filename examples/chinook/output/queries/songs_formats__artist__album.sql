038ba6876600dffe2519a816973a48410ded0350
Vineet Naik <naikvin@gmail.com>
2024-06-19 15:51:17 +0530

    Add back mistakenly deleted output query file
    In chinook example.

---
+++ examples/chinook/output/queries/songs_formats__artist__album.sql
@@ -0,0 +1,13 @@
+SELECT
+    track.name AS title,
+    artist.name AS artist_name,
+    album.title AS album_name,
+    media_type.name AS file_format
+FROM
+    album
+    JOIN artist USING (artist_id)
+    LEFT JOIN track USING (album_id)
+    JOIN media_type USING (media_type_id)
+WHERE
+    artist.name = $1;
+
